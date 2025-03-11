b446f31e01107445c6fc9315c8b8dbff03ea6fd4
Michael Sattler <michael.sattler@zuehlke.com>
2025-03-11 07:37:30 +0100

    Header update

--- WIDGETS/FlightMode/main.lua
+++ WIDGETS/FlightMode/main.lua
@@ -1,23 +1,17 @@
 ---------------------------------------------------------------------------
--- The dynamically loadable part of the Lua widget.                      --
+-- The main part of the Lua widget.                                      --
 --                                                                       --
--- Author:  Philippe Wechsler                                            --
--- Date:    2022-07-24                                                   --
+-- Author:  Michael Sattler                                              --
+-- Date:    2025-03-10                                                   --
 -- Version: 1.0.0                                                        --
--- Source: https://github.com/MadMonkey87/EdgeTX-Goodies                 --
+-- Source: https://github.com/Bastel-Bodo/EdgeTxWidgets                  --
+-- Derived from: https://github.com/MadMonkey87/EdgeTX-Goodies           --
 --                                                                       --
--- Copyright (C) Philippe Wechsler                                       --
---                                                                       --
--- License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html               --
+-- License: GPLv3                                                        --
 --                                                                       --
 -- This program is free software; you can redistribute it and/or modify  --
--- it under the terms of the GNU General Public License version 2 as     --
+-- it under the terms of the GNU General Public License version 3 as     --
 -- published by the Free Software Foundation.                            --
---                                                                       --
--- This program is distributed in the hope that it will be useful        --
--- but WITHOUT ANY WARRANTY; without even the implied warranty of        --
--- MERCHANTABILITY or FITNESS FOR borderON PARTICULAR PURPOSE. See the   --
--- GNU General Public License for more details.                          --
 ---------------------------------------------------------------------------
 
 local name = "FlightMode"
